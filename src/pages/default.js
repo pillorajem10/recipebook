1ca055b5cee756d6e331768691be6241d4740097
jempillora <>
2020-12-18 14:42:55 +0800

    modified default page

--- src/pages/default.js
+++ src/pages/default.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const About = (props) => {
   return (
-    <center className = 'default'>
-        Sorry page not found
-    </center>
+    <>
+      <center className = 'default'>404</center>
+      <center className = 'default'>Sorry but</center>
+      <center className = 'default text-danger'>{props.location.pathname}</center>
+      <center className = 'default'>Cannot be found</center>
+    </>
   )
 }
 
