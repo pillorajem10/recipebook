0106acdcfa8d77a30e3f4969da74472f31dd1449
unknown <>
2020-12-28 21:21:57 +0800

    added stylings

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { BottomNavigation } from '@material-ui/core';
+
+const Footer = () => {
+  return (
+    <footer className = 'footer' >All rights reserved.</footer>
+  )
+}
+
+export default Footer;
